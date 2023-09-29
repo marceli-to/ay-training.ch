98b66e9ab38d26abf20aa4009cf60830ee7ce3f3
Marcel Stadelmann <m@marceli.to>
2023-09-29 09:34:14 +0200

    wip

--- resources/views/components/media/picture.blade.php
+++ resources/views/components/media/picture.blade.php
@@ -1,9 +1,9 @@
 @props(['image', 'alt' => ''])
 <picture class="w-full">
-  <source media="(min-width: 768px)" srcset="/media/{{ $image }}.png">
-  <source srcset="/media/{{ $image }}-sm.png">
+  {{-- <source media="(min-width: 768px)" srcset="/media/{{ $image }}.png">
+  <source srcset="/media/{{ $image }}-sm.png"> --}}
   <img 
-    src="/media/{{ $image }}-sm.png" 
+    src="/media/{{ $image }}.jpg" 
     alt="{{ $alt }}" 
     title="{{ $alt }}" 
     height="1024" 
