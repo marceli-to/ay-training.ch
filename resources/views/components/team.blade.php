81650c962e0ef1ef992adb2fa4b69ac4e9fff492
Marcel Stadelmann <m@marceli.to>
2023-09-29 14:26:23 +0200

    wip

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -3,7 +3,7 @@
   <figure class="col-span-3 lg:col-span-3">
     <x-media.picture image="{{ $image }}" alt="{{ $name }}" class="rounded-full" />
   </figure>
-  <div class="col-span-9 lg:col-span-9 mt-16 xl:mt-24">
+  <div class="col-span-9 lg:col-span-9 sm:mt-16 xl:mt-24">
     <x-heading.h3 class="!mb-0">
       {{ $name}}
     </x-heading.h3>
